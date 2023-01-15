17d8aeb4a79ecfc78e53633d431832c4e5cd59e7
srijarkoroy <sr8962@srmist.edu.in>
2023-01-15 18:13:16 +0530

    feat: setting up linkseg module

---
+++ linkseg/__init__.py
@@ -0,0 +1,11 @@
+from .loss import DiceLoss, IoU
+from .linknet import LinkNet
+from .training_utils import Train, Evaluate
+
+__all__ = [
+    DiceLoss,
+    IoU,
+    LinkNet,
+    Train,
+    Evaluate,
+]
