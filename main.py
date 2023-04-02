1d126a5a1df6244bb047f5edea4b8d2f172f95ac
srijarkoroy <sr8962@srmist.edu.in>
2023-04-02 12:21:13 +0530

    feat: literature review

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from inference import LinkNetSeg
 
 from linkseg import LinkNet, DiceLoss, IoU, Train, Evaluate
 from utils.home import *
+from utils.review import *
 from utils.architecture import *
 from utils.visualizer import *
 from utils.trainer import *
 
-opt = st.sidebar.selectbox("Main",("Home", "Architecture", "Visualizer", "Train"), label_visibility="hidden")
+opt = st.sidebar.selectbox("Main",("Home", "Literature Review", "Architecture", "Visualizer", "Train"), label_visibility="hidden")
 
 if opt == "Home":
 
     home()
 
+elif opt == "Literature Review":
+
+    literature_review()
 
 elif opt == "Architecture":
 
@@ -47,30 +51,35 @@ elif opt == "Visualizer":
 
 elif opt == "Train":
 
-    path = file_upload()
+    try:
 
-    if path:
+        path = file_upload()
 
-        train_loader, val_loader = dataloader()
+        if path:
 
-        # Training and Evaluate object
-        train = Train(dice=DiceLoss(), iou=IoU())
-        eval = Evaluate(dice=DiceLoss(), iou=IoU())
+            train_loader, val_loader = dataloader()
 
-        # Model Initialization
-        model = LinkNet()
-        print(model)
+            # Training and Evaluate object
+            train = Train(dice=DiceLoss(), iou=IoU())
+            eval = Evaluate(dice=DiceLoss(), iou=IoU())
 
-    if train_loader and val_loader:
-        optimizer, lr, epochs= hyperparameters(model.parameters())
-        st.info("Optimizer: {}\n\nLearning Rate: {}\n\nEpochs: {}".format(optimizer, lr, epochs))
+            # Model Initialization
+            model = LinkNet()
+            print(model)
 
-        if lr != 0:
+        if train_loader and val_loader:
+            optimizer, lr, epochs= hyperparameters(model.parameters())
+            st.info("Optimizer: {}\n\nLearning Rate: {}\n\nEpochs: {}".format(optimizer, lr, epochs))
 
-            if st.button("Train!"):
+            if lr != 0:
 
-                for epoch in range(epochs):
-                    train_dice, train_iou = train.forward(model=model, loader=train_loader, optimizer=optimizer)
-                    val_dice, val_iou = eval.forward(model=model, loader=val_loader)
+                if st.button("Train!"):
 
-                    st.write("Epoch: {}, Dice Loss: {}, IoU Loss: {}".format(epoch, train_dice, train_iou))
+                    for epoch in range(epochs):
+                        train_dice, train_iou = train.forward(model=model, loader=train_loader, optimizer=optimizer)
+                        val_dice, val_iou = eval.forward(model=model, loader=val_loader)
+
+                        st.write("Epoch: {}, Dice Loss: {}, IoU Loss: {}".format(epoch, train_dice, train_iou))
+    
+    except Exception as e:
+        pass
