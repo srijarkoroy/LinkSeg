9967614a5e05f4b95b32ac0b31b9e918d72e3249
srijarkoroy <sr8962@srmist.edu.in>
2023-04-04 19:41:20 +0530

    style: annotation image

--- utils/home.py
+++ utils/home.py
@@ -16,6 +16,9 @@ def home():
     st.markdown(html_temp, unsafe_allow_html=True)
     st.header("")
 
+    st.image("docs/dataloader/annot.png", caption="DRIVE Dataset Annotations") 
+
+
     prob = st.checkbox("Problem")
 
     if prob:
@@ -25,6 +28,40 @@ def home():
 
     if obj:
         st.write("Implementations of a Semantic Segmentation Network to accurately segment the Blood Vessels present in the retina for proper treatment and reduction of operator fatigue.")
+
+    limit = st.checkbox("Research Gap")
+
+    if limit:
+        st.write("Existing systems for Retinal Blood Vessel Segmentation such as UNETs, CNNs, SVMs have a common problem of the loss of important spatial information during the feature extraction stage as a result of multiple downsampling. Hence, the existing models are not able to achieve the accuracy that it could have if the spatial information has been retained.Certain networks such as UNETs use Transpose Convolutions for the Upsampling task. The usage of Transpose Convolution often leads to noisy outputs.")
+
+    proposal = st.checkbox("Proposal")
+
+    if proposal:
+        st.markdown(
+            """
+            In order to counter the problem of lost spatial information, we propose using an improved LinkNet based architecture for semantic segmentation of Retinal Blood Vessels.
+
+            - The network makes use of skip connections to link each Encoder Block to its corresponding Decoder Block, passing on the lost spatial information to be concatenated during Upsampling.
+
+            - Using Upsample instead of Transpose Conv counters the problem of noisy output.
+            """)
+    
+    req = st.checkbox("Requirements")
+
+    if req:
+        st.markdown(
+            """
+            Software Requirements:
+            - Google Colab for Training
+            - Visual Studio Code
+            - Linux Terminal for local Testing
+            - Python
+            - PyTorch
+
+            Hardware Requirements:
+            - NVIDIA Tesla T4 GPU
+            - cuDNN VERSION 8005
+            """)
 
     res = st.checkbox("Check out the Results of our Implementation!")
 
