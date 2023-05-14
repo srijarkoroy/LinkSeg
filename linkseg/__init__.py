ff0bf3accd993d462541d05392bd48f0912b9127
srijarkoroy <sr8962@srmist.edu.in>
2023-05-14 16:02:01 +0530

    feat: setup PixelAccuracy(metric) in linkseg module

--- linkseg/__init__.py
+++ linkseg/__init__.py
@@ -1,10 +1,11 @@
-from .loss import DiceLoss, IoU
+from .loss import DiceLoss, IoU, PixelAccuracy
 from .linknet import LinkNet
 from .training_utils import Train, Evaluate
 
 __all__ = [
     DiceLoss,
     IoU,
+    PixelAccuracy,
     LinkNet,
     Train,
     Evaluate,
